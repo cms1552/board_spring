61cae23a100a427a6b5bc978f66092bbae694a12
cms <a49351552@gmail.com>
2023-02-13 17:54:06 +0900

    레디스 유틸 클래스 생성

---
+++ src/main/java/hello/board/utils/RedisUtil.java
@@ -0,0 +1,38 @@
+package hello.board.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+    private final StringRedisTemplate redisTemplate;
+
+    // key -> value
+    public String getData(String key) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    // 저장
+    public void setDate(String key, String value) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, value);
+    }
+
+    // 유효 시간 동안 (key, value) 저장
+    public void setDateExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    // 삭제
+    public void deleteDate(String key) {
+        redisTemplate.delete(key);
+    }
+}
