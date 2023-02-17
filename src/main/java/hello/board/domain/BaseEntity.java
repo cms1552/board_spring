8a9f04b2cb249c0a6a1bd99340d05799721190ca
cms <a49351552@gmail.com>
2023-02-17 17:41:42 +0900

    BaseEntity createdAt, updatedAt 날짜 형식 변경

--- src/main/java/hello/board/domain/BaseEntity.java
+++ src/main/java/hello/board/domain/BaseEntity.java
@@ -5,20 +5,36 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @MappedSuperclass
 @Getter
 @EntityListeners(value = {AuditingEntityListener.class})
 public class BaseEntity {
 
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createAt;
+//    @CreatedDate
+//    @Column(updatable = false)
+//    private LocalDateTime createAt;
 
-    @LastModifiedDate
-    private LocalDateTime updateAt;
+    @Column(updatable = false)
+    private String createAt;
+
+//    @LastModifiedDate
+//    private LocalDateTime updateAt;
+
+    private String updateAt;
+
+    @PrePersist // 엔티티 저장하기 전
+    void onPrePersist() {
+        this.createAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.updateAt = createAt;
+    }
+
+    @PreUpdate  // 엔티티 수정하기 전
+    void onPreUpdate() {
+        this.updateAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
 }
